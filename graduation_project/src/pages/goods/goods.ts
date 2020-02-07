5afcd99b5eccbf1711b4f00a7f088e74bf191961
zxy5663 <1976871412@qq.com>
2020-02-07 21:15:05 +0800

    修改商品页

--- graduation_project/src/pages/goods/goods.ts
+++ graduation_project/src/pages/goods/goods.ts
@@ -21,8 +21,14 @@ export class GoodsPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad GoodsPage');
   }
+
   isActive=1; //用于标记s商品页的3个导航按钮， 0表示商品 1表示详情 2表示评论
+  imgs=['01','02','03','04','05','06','07','08','09','010','011','012','013','015','016','017','018','019','020'];
+
   isClick(i){
     this.isActive=i;
+  }
+  goHome(){
+    this.navCtrl.pop();
   }
 }
