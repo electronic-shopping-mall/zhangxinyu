a3d7f20d07565a1b5df8d4f07ede32d0c406c476
zxy5663 <1976871412@qq.com>
2020-02-16 20:58:37 +0800

    修改咨询详情页

--- graduation_project/src/pages/informationdetail/informationdetail.ts
+++ graduation_project/src/pages/informationdetail/informationdetail.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
 
 /**
  * Generated class for the InformationdetailPage page.
@@ -15,11 +16,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class InformationdetailPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,private http:HttpClient) {
   }
 
+  newsID; //标记点开的是哪条资讯
+  newsData;  //从后台返回的资讯数据
+  pictures;
+  contents;
+  title;
   ionViewDidLoad() {
-    console.log('ionViewDidLoad InformationdetailPage');
+    this.newsID=localStorage.getItem('newsID');
+    console.log(this.newsID);
+    this.http.post('/api/news/specific',{"newsID":this.newsID}).subscribe(data=>{
+      this.newsData=Array.prototype.slice.call(data);
+      this.pictures=this.newsData[0].pictures.split('|').slice(1);
+      this.contents=this.newsData[0].content.split('|');
+      this.title=this.newsData[0].title;
+    });
   }
   goback(){
     this.navCtrl.pop();
