f25e7739f693b736381391b974cd83209e7b8476
zxy5663 <1976871412@qq.com>
2020-02-16 20:56:09 +0800

    增加我的收货地址页

---
+++ graduation_project/src/pages/address/address.ts
@@ -0,0 +1,95 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams,AlertController } from 'ionic-angular';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
+
+import { AddAddressPage } from '../add-address/add-address';
+import { EditAddressPage } from '../edit-address/edit-address';
+
+/**
+ * Generated class for the AddressPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-address',
+  templateUrl: 'address.html',
+})
+export class AddressPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private http:HttpClient,public alertCtrl: AlertController) {
+  }
+
+  show; //标记两种页面显示 地址为空和地址不为空
+  userID;  //用于标记当前是哪个用户的登录
+  addressData; //从后台返回的某用户的收获地址
+
+  //刚进入页面时触发
+  ionViewWillEnter() {
+    this.userID=localStorage.getItem('userID');
+    this.http.post("/api/address",{"userID":this.userID}).subscribe(data=>{
+      this.addressData=Array.prototype.slice.call(data);
+      if(this.addressData.length==0){
+        this.show=1;
+      }else{
+        this.show=2;
+      }
+    });
+  }
+
+  goAdd(){
+    this.navCtrl.push(AddAddressPage);
+  }
+
+  changeDefault(i){ //修改默认地址
+    if(this.addressData[i].isDefault=='false'){
+      this.addressData[i].isDefault='true';
+      this.http.post('/api/changeDefault',{'addressID':this.addressData[i].addressID,'userID':this.userID,'isDefault':this.addressData[i].isDefault}).subscribe(data=>{
+      });
+      for(var j=0;j<this.addressData.length;j++){
+        if(j!=i){
+          this.addressData[j].isDefault='false';
+        }
+      }
+    }else{
+      this.addressData[i].isDefault='false';
+      this.http.post('/api/changeDefault',{'addressID':this.addressData[i].addressID,'userID':this.userID,'isDefault':this.addressData[i].isDefault}).subscribe(data=>{
+      });
+    }
+
+  }
+
+  edit(i){  //编辑地址
+    localStorage.setItem('addressID',this.addressData[i].addressID);
+    this.navCtrl.push(EditAddressPage);
+  }
+
+  delete(addressID,i){  //删除地址
+    console.log(this.addressData[i].addressID);
+    const alert =this.alertCtrl.create({
+      title: '确定删除该收货地址吗？',
+      message: '   ',
+      buttons: [
+        {
+          text: '取消',
+          cssClass: 'secondary',
+          // handler: (blash) => {
+          //   console.log('Confirm Cancel: blah',blash);
+          // }
+        }, {
+          text: '确认',
+          handler: () => {
+            console.log('Confirm Okay');
+            console.log(addressID);
+            this.http.post('/api/deleteAddress',{"addressID":addressID,"userID":this.userID}).subscribe(data=>{
+              this.addressData=Array.prototype.slice.call(data);
+            });
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+}
