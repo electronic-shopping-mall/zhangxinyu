a0e70332dde2021a4a89337094c8b23c1c4553c0
zxy5663 <1976871412@qq.com>
2020-03-29 11:09:22 +0800

    资讯详情页

---
+++ eShoppingMall/src/pages/informationdetail/informationdetail.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { InformationdetailPage } from './informationdetail';
+
+@NgModule({
+  declarations: [
+    InformationdetailPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(InformationdetailPage),
+  ],
+})
+export class InformationdetailPageModule {}
