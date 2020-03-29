1db6f865bcddb2ab675abd017b78dcd3ce8095d8
zxy5663 <1976871412@qq.com>
2020-03-29 10:58:41 +0800

    添加银行卡页

---
+++ eShoppingMall/src/pages/addbankcard/addbankcard.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AddbankcardPage } from './addbankcard';
+
+@NgModule({
+  declarations: [
+    AddbankcardPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AddbankcardPage),
+  ],
+})
+export class AddbankcardPageModule {}
