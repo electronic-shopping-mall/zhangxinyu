46b7185f519fd34ccdbd5110125d031304b48048
zxy5663 <1976871412@qq.com>
2020-03-31 10:05:46 +0800

    充值页

---
+++ eShoppingMall/src/pages/topup/topup.ts
@@ -0,0 +1,94 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams,AlertController } from 'ionic-angular';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
+import { BankcardManagePage } from '../bankcard-manage/bankcard-manage';
+
+/**
+ * Generated class for the TopupPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-topup',
+  templateUrl: 'topup.html',
+})
+export class TopupPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private http:HttpClient,public alertCtrl: AlertController) {
+  }
+
+  userID;
+  userBalance;
+  chooseBankCardData;//银行卡选择页面返回的用户选择的银行卡信息
+  bankcard='请选择银行卡'; //用户选择的银行卡信息
+  cardnumber;
+  ionViewWillEnter() {
+    this.userID=localStorage.getItem("userID");
+    this.chooseBankCardData=this.navParams.get('chooseBankCardData');
+    if(this.chooseBankCardData!=undefined){
+      this.cardnumber=this.chooseBankCardData[0].cardnumber.replace(/\s/g, "");
+      this.cardnumber=this.cardnumber.substring(this.cardnumber.length-4);
+      this.bankcard=this.chooseBankCardData[0].bankname+'（'+this.cardnumber+'）';
+      console.log(this.cardnumber);
+    }
+    this.http.post('/api/userinfo',{"userID":this.userID}).subscribe(data=>{
+      this.userBalance=Array.prototype.slice.call(data)[0].userBalance;
+    });
+  }
+  getfocus(){
+    var inp=document.getElementById("inp");
+    console.log(inp);
+    inp.focus();
+  }
+  //控制弹窗的参数信息
+  IsShowDialogTwo: boolean = false;
+  IsShowCover: boolean = false;
+  money;
+  sure(){
+    console.log(this.money);
+    if(this.chooseBankCardData==undefined){
+      const alert1= this.alertCtrl.create({
+        title: '请选择银行卡',
+        message: '',
+        buttons: ['确定']
+      });
+      alert1.present();
+    }
+    else if(this.money==undefined){
+      const alert2= this.alertCtrl.create({
+        title: '请输入充值金额',
+        message: '',
+        buttons: ['确定']
+      });
+      alert2.present();
+    }
+    else{
+      var endMoney=this.money+this.userBalance;
+      this.http.post('/api/topup',{"userID":this.userID,"money":endMoney}).subscribe(data=>{
+        if(data['status']==1){
+          console.log('充值成功');    
+          this.IsShowDialogTwo=true;   
+          this.IsShowCover = true;
+          var that=this;
+          setTimeout(function(){
+            if (that.IsShowDialogTwo) {
+              that.IsShowDialogTwo = false;
+              that.IsShowCover = false;
+            }
+            that.navCtrl.pop();
+          },1000);
+        }
+      });
+    }
+  }
+  goBack(){
+    this.navCtrl.pop();
+  }
+  chooseBankCard(){
+    localStorage.setItem("isChooseBankCard",'1');
+    this.navCtrl.push(BankcardManagePage);
+  }
+}
